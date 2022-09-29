8804c59aaf79427113a569069108c12fa0af550d
MuhammedTafli <koska_33@hotmail.com>
2022-09-29 16:28:19 +0300

    en büyük sayı projesi tamamlandı.

--- enBuyukSayi/Main.java
+++ enBuyukSayi/Main.java
@@ -7,12 +7,12 @@ public class Main {
         int sayi3 = 2341;
         int enBuyuk = sayi1;
 
-        if(enBuyuk<sayi2){
-            enBuyuk=sayi2;
+        if (enBuyuk < sayi2) {
+            enBuyuk = sayi2;
         }
-        if(enBuyuk<sayi3){
-            enBuyuk=sayi3;
+        if (enBuyuk < sayi3) {
+            enBuyuk = sayi3;
         }
-        System.out.println("En büyük sayımız = "+enBuyuk);
+        System.out.println("En büyük sayımız = " + enBuyuk);
     }
 }
